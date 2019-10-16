545091c2901c3a5b7e50ebb66c6d90c17e261089
mholmes <holmesmarkr@johndeere.com>
2019-10-16 10:44:12 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,27 @@
 
 
 
-var cardOne = "queen";
-var cardTwo = "king";
+
+
+
+var cards = ["queen","queen","king","king"]
+var cardsInPlay = []
+var cardOne = cards[0]
+var cardTwo = cards[2]
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+
+	if (length.cardsInPlay === 2)
+		{}
+	if(cardsInPlay[0] === cardsInPlay[1])
+		{alert("You found a match")}
+	else
+		{alert("Sorry, try again")}
+
+
+
 
 
 
