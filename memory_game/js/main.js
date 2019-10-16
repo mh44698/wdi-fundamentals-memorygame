f8fb57749069e32773a305a2c96531bf18286b68
mholmes <holmesmarkr@johndeere.com>
2019-10-16 14:44:45 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,29 +1,62 @@
+var cards = [
+          {
+                   rank: "queen",
+                   suit: "hearts",
+                   image: "images/queen-of-hearts.png"
+          },
+          {
+                   rank: "queen",
+                   suit: "diamonds",
+                   image: "images/queen-of-diamonds.png"
+          },
+          {
+                   rank: "king",
+                   suit: "hearts",
+                   image: "images/king-of-hearts.png"
+          },
+          {
+                   rank: "king",
+                   suit: "diamonds",
+                   image: "images/king-of-diamonds.png"
+          }
+];
+
+var cardsInPlay = [];
+
+function checkForMatch() {
+          if (cardsInPlay[0] === cardsInPlay[1]) {
+                   alert("You found a match!");
+} else {
+                   alert("Sorry, try again.");
+}
+}
+function flipCard() {
+          var cardID = this.getAttribute('data-id');
+          cardsInPlay.push(cards[cardID].rank);
+          console.log("User flipped " + cards[cardID].rank);
+          console.log(cards[cardID].suit);
+          console.log(cards[cardID].image);
+          this.setAttribute("src", cards[cardId].cardImage)    
+          if (cardsInPlay.length === 2)  {
+                   checkForMatch();
+          }        
+}
+
+function createBoard() {
+          for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src',"images/back.png");
+    cardElement.setAttribute('data-id',i);
+    cardElement.addEventListener("click", flipCard);
+    var gb = document.getElementById('game-board');
+    gb.appendChild(cardElement);
+}
+}
+
+createBoard();
+	
 
 
 
 
 
-
-var cards = ["queen","queen","king","king"]
-var cardsInPlay = []
-var cardOne = cards[0]
-var cardTwo = cards[2]
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-
-	if (length.cardsInPlay === 2)
-		{}
-	if(cardsInPlay[0] === cardsInPlay[1])
-		{alert("You found a match")}
-	else
-		{alert("Sorry, try again")}
-
-
-
-
-
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
